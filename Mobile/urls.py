180ae9c3160e93f762c8f6a32ecd1292fb9ba6d4
yogesh-shinde <shinde_yogeshr@yahoo.com>
2020-11-10 12:29:10 +0530

    change in url

--- Mobile/urls.py
+++ Mobile/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('company', Company_Create.as_view(), name='company-create'),
-    path('model', Model_Create.as_view(), name='model-create'),
+    path('company/', Company_Create.as_view(), name='company-create'),
+    path('model/', Model_Create.as_view(), name='model-create'),
 
 ]
